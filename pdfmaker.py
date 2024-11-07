39bd9b96e8f5f1fd730c83aab59876c41f38b239
P. Michael Kielstra <4313413+PMKielstra@users.noreply.github.com>
2024-11-07 15:17:24 -0800

    Bugfix and move to pdfrw

--- pdfmaker.py
+++ pdfmaker.py
@@ -1,8 +1,16 @@
 import csv
 from tempfile import TemporaryDirectory
 from os import path, makedirs
-from pypdf import PdfWriter
+# from pypdf import PdfWriter
+from pdfrw import PdfReader, PdfWriter, PdfName, PdfDict
 from md2pdf.core import md2pdf
+
+def blank_page(template):
+    x = PdfDict()
+    x.Type = PdfName.Page
+    x.Contents = PdfDict(stream="")
+    x.MediaBox = template.inheritable.MediaBox
+    return x
 
 def process_csv(in_file, out_file):
     with TemporaryDirectory() as tmp_path:
@@ -10,7 +18,8 @@ def process_csv(in_file, out_file):
         csvreader = csv.reader(in_file)
         headers = csvreader.__next__()
 
-        pdf = PdfWriter()
+        pdf = PdfWriter(out_file)
+        bp = None
 
         for rownum, row in enumerate(csvreader, start=2):
             row_text = '\n\n'.join([f'## Row: {rownum}'] + [
@@ -20,9 +29,14 @@ def process_csv(in_file, out_file):
             ])
             pdf_path = path.join(tmp_path, f'{rownum}.pdf') # Use a named file in a temp directory to avoid potential weirdness with closing and re-opening
             md2pdf(pdf_file_path=pdf_path, md_content=row_text)
-            
-            pdf.append(pdf_path)
-            if len(pdf.pages) % 2 != 0:
-                pdf.add_blank_page()
 
-        pdf.write(out_file)
+            pages = PdfReader(pdf_path).pages
+            
+            if bp is None:
+                bp = blank_page(pages[0])
+
+            pdf.addpages(pages)
+            if len(pages) % 2 != 0:
+                pdf.addpage(bp)
+
+        pdf.write()
