d5bd1838d35d79b42c4ebed445120aba2a81f64c
P. Michael Kielstra <4313413+PMKielstra@users.noreply.github.com>
2024-11-14 11:35:07 -0800

    Better polling etc for long-running process

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from tempfile import TemporaryFile
 import io
 
 import flask
-from flask import request
+from flask import request, jsonify
+
+from flask_executor import Executor
 
 from pdfmaker import process_csv
 
 app = flask.Flask(__name__)
+
+executor = Executor(app)
 
 UPLOAD_FILE_NAME = "file"
 
@@ -14,12 +18,28 @@ UPLOAD_FILE_NAME = "file"
 def frontpage():
     return flask.render_template("index.html")
 
-@app.route("/render", methods=["POST"])
-def render():
-    if UPLOAD_FILE_NAME not in request.files:
-        return flask.redirect("/")
-    in_file = request.files[UPLOAD_FILE_NAME]
+RENDER_TASK_NAME = "pdf_render"
+
+def task_render(in_file):
     out_file = TemporaryFile()
     process_csv(io.TextIOWrapper(in_file, encoding="utf-8"), out_file)
+    in_file.close()
     out_file.seek(0)
+    return out_file
+
+@app.route("/start-render", methods=["POST"])
+def start_render():
+    if UPLOAD_FILE_NAME not in request.files:
+        return flask.redirect("/")
+    in_file = TemporaryFile()
+    request.files[UPLOAD_FILE_NAME].save(in_file)
+    in_file.seek(0)
+    executor.submit_stored(RENDER_TASK_NAME, task_render, in_file)
+    return {}
+
+@app.route("/get-render")
+def get_render():
+    if not executor.futures.done(RENDER_TASK_NAME):
+        return jsonify({"waiting": True})
+    out_file = executor.futures.pop(RENDER_TASK_NAME).result()
     return flask.send_file(out_file, mimetype="application/pdf", download_name="Printable.pdf")
