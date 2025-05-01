a51161d224942ea7f975e3a2ae054bea5882624c
P. Michael Kielstra <4313413+PMKielstra@users.noreply.github.com>
2025-04-30 22:16:26 -0700

    Add options for zip file and single-sided printing

--- app.py
+++ app.py
@@ -10,14 +10,16 @@ app = flask.Flask(__name__)
 executor = Executor(app)
 
 UPLOAD_FILE_NAME = "file"
+DOUBLESIDED_NAME = "doublesided"
+COMBINE_NAME = "combine"
 RENDER_TASK_NAME = "pdf_render"
 
-def task_render(in_file):
+def task_render(in_file, doublesided, combine):
     out_file = TemporaryFile()
-    process_csv(io.TextIOWrapper(in_file, encoding="utf-8"), out_file)
+    process_csv(io.TextIOWrapper(in_file, encoding="utf-8"), out_file, doublesided, combine)
     in_file.close()
     out_file.seek(0)
-    return out_file
+    return out_file, combine
 
 @app.route("/")
 def frontpage():
@@ -30,15 +32,20 @@ def start_render():
     in_file = TemporaryFile()
     request.files[UPLOAD_FILE_NAME].save(in_file)
     in_file.seek(0)
-    executor.submit_stored(RENDER_TASK_NAME, task_render, in_file)
+    print(request.form)
+    print(request.form.get(DOUBLESIDED_NAME))
+    print(request.form.get(COMBINE_NAME))
+    form_args = map(lambda n: request.form.get(n) == "on", (DOUBLESIDED_NAME, COMBINE_NAME))
+    executor.submit_stored(RENDER_TASK_NAME, task_render, in_file, *form_args)
     return {}
 
 @app.route("/get-render")
 def get_render():
     if not executor.futures.done(RENDER_TASK_NAME):
         return jsonify({"waiting": True})
-    out_file = executor.futures.pop(RENDER_TASK_NAME).result()
-    return flask.send_file(out_file, mimetype="application/pdf", download_name="Printable.pdf")
+    out_file, is_pdf = executor.futures.pop(RENDER_TASK_NAME).result()
+    ext = "pdf" if is_pdf else "zip"
+    return flask.send_file(out_file, mimetype="application/{0}".format(ext), download_name="Printable.{0}".format(ext))
 
 if __name__ == "__main__":
     app.run()
--- pdfmaker.py
+++ pdfmaker.py
@@ -1,6 +1,7 @@
 import csv
 from tempfile import TemporaryDirectory
 from os import path
+from zipfile import ZipFile
 from pdfrw import PdfReader, PdfWriter, PdfName, PdfDict
 from md2pdf.core import md2pdf
 
@@ -11,13 +12,17 @@ def blank_page(template):
     x.MediaBox = template.inheritable.MediaBox
     return x
 
-def process_csv(in_file, out_file):
+def process_csv(in_file, out_file, doublesided, combine):
     if not in_file: return        
     with TemporaryDirectory() as tmp_path:
         csvreader = csv.reader(in_file)
         headers = csvreader.__next__()
 
-        pdf = PdfWriter(out_file)
+        if combine:
+            pdf = PdfWriter(out_file)
+        else:
+            zip = ZipFile(out_file, "w")
+
         bp = None
 
         for rownum, row in enumerate(csvreader, start=2):
@@ -26,16 +31,24 @@ def process_csv(in_file, out_file):
                 for i, elt in enumerate(row)
                 if elt != '' and headers[i][0] != '-'
             ])
-            pdf_path = path.join(tmp_path, '{0}.pdf'.format(rownum)) # Use a named file in a temp directory to avoid potential weirdness with closing and re-opening
+            pdf_name = '{0}.pdf'.format(rownum)
+            pdf_path = path.join(tmp_path, pdf_name) # Use a named file in a temp directory to avoid potential weirdness with closing and re-opening
             md2pdf(pdf_file_path=pdf_path, md_content=row_text)
 
+            if not combine:
+                zip.write(pdf_path, pdf_name)
+                continue
+
             pages = PdfReader(pdf_path).pages
-            
-            if bp is None:
-                bp = blank_page(pages[0])
-
             pdf.addpages(pages)
-            if len(pages) % 2 != 0:
-                pdf.addpage(bp)
+            if doublesided:
+                if bp is None:
+                    bp = blank_page(pages[0])
 
-        pdf.write()
+                if len(pages) % 2 != 0:
+                    pdf.addpage(bp)
+
+        if combine:
+            pdf.write()
+        else:
+            zip.close()
